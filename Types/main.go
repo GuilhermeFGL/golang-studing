d2052c47995ae15b5bcf206dc96cd416641d6d4e
Guilherme  Lima <guilherme.lima@avenuecodes-MacBook-Pro.local>
2025-03-06 14:32:17 -0300

    variables and types

---
+++ Types/main.go
@@ -0,0 +1,17 @@
+package Types
+
+import "fmt"
+
+func main() {
+
+	// numbers
+	var n int = 100000 //int8, int32, unint ...
+	var b byte = 1
+	var f float32 = 1 // float64
+
+	// text
+	var s string = "hello world"
+	var c = 'c' // char
+
+	fmt.Println(n, b, f, s, c, " ")
+}
