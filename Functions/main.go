8626d694eca4096fae20f9dd2f4fe502071d6689
Guilherme  Lima <guilherme.lima@avenuecodes-MacBook-Pro.local>
2025-03-06 17:35:45 -0300

    functions

--- Functions/main.go
+++ Functions/main.go
@@ -10,10 +10,19 @@ func switchNumbers(n1, n2 int) (int, int) {
 	return n2, n1
 }
 
+func mappedReturn(n1, n2 int) (r1 int, r2 int) {
+	r1 = n2
+	r2 = n1
+	return
+}
+
 func main() {
 	fmt.Println(add(1, 2))
 
 	fmt.Println(switchNumbers(1, 2))
+
+	r1, r2 := mappedReturn(1, 2)
+	fmt.Println(r1, r2)
 
 	internalFunction := func(text string) {
 		fmt.Println(text)
