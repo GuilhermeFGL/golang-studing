34297833bb8a873517e6522a086dc7c498ced919
Guilherme  Lima <guilherme.lima@avenuecodes-MacBook-Pro.local>
2025-03-06 17:59:10 -0300

    even more functions

--- Functions/main.go
+++ Functions/main.go
@@ -16,7 +16,28 @@ func mappedReturn(n1, n2 int) (r1 int, r2 int) {
 	return
 }
 
+func multipleParams(numbers ...int) (total int) {
+	for number := range numbers {
+		total += number
+	}
+	return
+}
+
+func recursive(n int) int {
+	if n > 1 {
+		return recursive(n-2) + recursive(n-1)
+	}
+	return n
+}
+
+func delayedExecution() {
+	defer fmt.Println("this will also be delayed")
+	fmt.Println("delayed execution")
+}
+
 func main() {
+	defer delayedExecution() // will be executed at the end of the scope
+
 	fmt.Println(add(1, 2))
 
 	fmt.Println(switchNumbers(1, 2))
@@ -24,8 +45,18 @@ func main() {
 	r1, r2 := mappedReturn(1, 2)
 	fmt.Println(r1, r2)
 
+	fmt.Println(multipleParams(1, 2, 3, 4, 5, 6, 7, 7, 9))
+
 	internalFunction := func(text string) {
 		fmt.Println(text)
 	}
 	internalFunction("hello world from a function")
+
+	fmt.Println("fibonacci:", recursive(10))
+
+	result := func(text string) string {
+		println("Anonymous function:", text) // will not be printed
+		return text
+	}("executing") // declare and execute the function
+	fmt.Println(result)
 }
