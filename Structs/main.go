583c9c4f6dac3a5a54a46607d7e5c018e27ac036
Guilherme  Lima <guilherme.lima@avenuecodes-MacBook-Pro.local>
2025-03-06 15:09:27 -0300

    structs

---
+++ Structs/main.go
@@ -0,0 +1,39 @@
+package main
+
+import "fmt"
+
+type user struct {
+	name    string
+	age     uint8
+	address address
+}
+
+type address struct {
+	street string
+	number int
+}
+
+func main() {
+	var myUser1 = user{
+		name: "Jack",
+		age:  23,
+		address: address{
+			street: "123",
+			number: 201,
+		},
+	}
+
+	var myUser2 user
+	myUser2.name = "David"
+	myUser2.age = 21
+	myUser2.address = address{}
+	myUser2.address.street = "Avenue A"
+	myUser2.address.number = 500
+
+	myUser3 := user{"Ana", 19, address{"street", 404}}
+
+	var myUser4 user
+	var myUser5 = user{}
+
+	fmt.Println(myUser1, myUser2, myUser3, myUser4, myUser5, " ")
+}
