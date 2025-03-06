cf3eb279725eb6bb2e0f9f9b6d0779a105aa4b03
Guilherme  Lima <guilherme.lima@avenuecodes-MacBook-Pro.local>
2025-03-06 17:32:24 -0300

    loops

---
+++ Loops/main.go
@@ -0,0 +1,33 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	for i := 0; i < 10; i++ {
+		fmt.Println(i)
+	}
+
+	n := 0
+	for n < 10 {
+		fmt.Println(n)
+		n++
+	}
+
+	texts := [3]string{"a1", "a2", "a3"}
+	for i, t := range texts {
+		fmt.Println(i, t)
+	}
+
+	for i, t := range "TEXT" {
+		fmt.Println(i, string(t))
+	}
+
+	myMap := map[string]int{
+		"property1": 1,
+		"property2": 2,
+	}
+	for k, v := range myMap {
+		fmt.Println(k, v)
+	}
+}
